0c52115bd960673ccbe3800bf4dd18135ea4a7db
Grindmeow <thehumaneatermeow@gmail.com>
2024-01-20 23:42:51 +0545

    changes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,104 +1,60 @@
-import React from 'react'
-import Header from '../components/Header'
-import {
-  Carousel,
-  Button
-} from '@material-tailwind/react'
+import Slider from "react-slick"
+import "../Styles/HomeSlider.css"
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import { Button } from "@material-tailwind/react";
+
 
 const Homepage = () => {
+  const settings = {
+    fade: false,
+    speed: 1000,
+    autoplaySpeed: 5000,
+    infinite: true,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+  };
   return (
-    <div>
-      <Carousel >
+    <>
+      <div className="absolute  h-full w-full bg-black/0 z-10">
+        <Header />
+        <div className='grid items-center justify-center mt-[155px] space-y-7'>
+          <h1 className='text-white text-4xl font-medium ' >Easiest way to find your dream home</h1>
+
+          <div className='relative left-3'><input className='rounded-full bg-white text-black outline-none border-none w-[400px] p-[12px]'
 
 
-        <div>
+            placeholder='Your ZIP code or City e.g. Dharan'></input>
 
-          <img
-            src="../images/property2.avif"
-            alt="image 1"
-            className="h-[100vh] w-[100vw] object-cover"
-          />
+            <Button className='ml-2 bg-teal-900 rounded-full'
+              size='lg'
 
-          <div className="absolute inset-0 h-full w-full bg-black/0">
-            <Header />
-            <div className='grid items-center justify-center mt-[155px] space-y-7'>
-              <h1 className='text-white text-4xl font-medium ' >Easiest way to find your dream home</h1>
-
-              <div className='relative left-3'><input className='rounded-full bg-white text-black outline-none border-none w-[400px] p-[12px]'
-
-
-                placeholder='Your ZIP code or City e.g. Dharan'></input>
-
-                <Button className='ml-2 bg-teal-900 rounded-full'
-                  size='lg'
-
-                >Search</Button>
-              </div>
-            </div>
+            >Search</Button>
           </div>
         </div>
+      </div>
 
 
-        <div>
-
-          <img
-            src="../images/property3.avif"
-            alt="image 2"
-            className="h-[100vh] w-[100vw] object-cover"
-          />
-
-          <div className="absolute inset-0 h-full w-full bg-black/40">
-            <Header />
-
-            <div className='grid items-center justify-center mt-[155px] space-y-7'>
-              <h1 className='text-white text-4xl font-medium ' >Easiest way to find your dream home</h1>
-
-              <div className='relative left-3'><input className='rounded-full bg-white text-black outline-none border-none w-[400px] p-[12px]'
-
-
-                placeholder='Your ZIP code or City e.g. Dharan'></input>
-
-                <Button className='ml-2 bg-teal-900 rounded-full'
-                  size='lg'
-
-                >Search</Button>
-              </div>
-            </div>
-          </div>
+      <Slider {...settings} className="w-[100%] md:h-[100vh] h-[100vh] overflow-hidden ">
+        <div className="slider__item-01 h-[100vh]">
         </div>
 
-
-        <div>
-          <img
-            src="../images/property1.jpg"
-            alt="image 3"
-            className="h-[100vh] w-[100vw] object-cover"
-          />
-
-          <div className="absolute inset-0 h-full w-full bg-black/50">
-            <Header />
-
-            <div className='grid items-center justify-center mt-[155px] space-y-7'>
-              <h1 className='text-white text-4xl font-medium ' >Easiest way to find your dream home</h1>
-
-              <div className='relative left-3'><input className='rounded-full bg-white text-black outline-none border-none w-[400px] p-[12px]'
-
-
-                placeholder='Your ZIP code or City e.g. Dharan'></input>
-
-                <Button className='ml-2 bg-teal-900 rounded-full'
-                  size='lg'
-
-                >Search</Button>
-              </div>
-            </div>
-          </div>
+        <div className="slider__item-02  md:h-[100vh] h-[100vh]">
         </div>
-      </Carousel>
 
+        <div className="slider__item-03   md:h-[100vh] h-[100vh]">
 
-    </div>
+        </div>
+
+      </Slider>
+
+      <div>
+        <p>hi</p>
+      </div>
+
+    </>
   )
 }
-
 export default Homepage
